c25b700edfe8e05914d3296c14022d2f7d7b0795
Martin Wong <github@dev.martin0wong.com>
2026-04-26 01:22:45 -0700

    apparently not poggies with bad import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { motion, AnimatePresence } from "framer-motion";
